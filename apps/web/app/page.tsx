d9c9c492a2901e6c754dbc2abcb2b3803a8f87a5
Puskar-Roy <puskarroy600@gmail.com>
2024-06-01 20:38:51 +0530

    feat : background

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,40 @@
-export default function page(){
-    return (
+import Image from "next/image";
+import heroImage from "../assets/output_image.png";
+import logo from "../assets/3.png";
+import { Montserrat, Ubuntu } from "next/font/google";
+import '../assets/index.css'
 
-        <main>
+const alata = Montserrat({
+  weight: "800",
+  subsets: ["latin"],
+  display: "swap",
+});
+const ubuntu = Ubuntu({
+  weight: "700",
+  subsets: ["latin"],
+  display: "swap",
+});
 
-                <div className="">
-
-                    Hello From Server
-                </div>
-
-
-        </main>
-
-    )
-
+export default function page() {
+  return (
+    <main>
+      <div className="menu">
+        {/* <Image
+          className="w-screen h-screen absolute top-0 left-0 -z-[100]"
+          src={heroImage}
+          alt="hero"
+        /> */}
+        <div>
+          <div>
+            <Image
+              src={logo}
+              alt="logo"
+              className="h-[80px] w-auto rounded-full"
+            />
+            <h1 className={`${ubuntu.className}`}>Hello</h1>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
